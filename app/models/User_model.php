bca955378c9daedf20857d0a3d251bd6afb86616
Riza Pranata <riza.ari@dkatalis.com>
2020-02-02 23:14:26 +0700

    menambah model

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model{
+    private $nama = 'Riza Pranata';
+
+    public function getUser(){
+        return $this->nama;
+    }
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="jumbotron jumbotron-fluid mt-4">
         <div class="container">
-            <h1 class="display-4">Selamat Datang</h1>
+            <h1 class="display-4">Selamat Datang di Web <?= $data['nama']; ?></h1>
             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
         </div>
     </div>
